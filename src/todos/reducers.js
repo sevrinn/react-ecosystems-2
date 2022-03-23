bbab3de8d17ec785f79b35ec71036edc6ed7f099
Sevrinn <sevrinnsevrinn@gmail.com>
2022-03-23 15:46:54 -0700

    added comments

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,24 +1,27 @@
 import { CREATE_TODO, REMOVE_TODO } from './actions'
 
 export const todos = (state = [], action) => {
-	const { type, payload } = payload
-
+	const { type, payload } = action
+	//checks for action type from actions.js
 	switch (type) {
+		//gets text from payload, creates a new todo obj comprised of todo text and isComplete
 		case CREATE_TODO: {
 			const { text } = payload
 			const newTodo = {
 				text,
 				isCompleted: false,
 			}
+			//returns current state of todos with newTodo concat'd to the top of list (non destructive)
 			return state.concat(newTodo)
 		}
 
+		//gets text from payload, returns the current state with the todo.text filtered out (non destuctive)
 		case REMOVE_TODO: {
 			const { text } = payload
 
 			return state.filter((todo) => todo.text !== text)
 		}
-
+		// default case just returns the current state
 		default: {
 			return state
 		}
